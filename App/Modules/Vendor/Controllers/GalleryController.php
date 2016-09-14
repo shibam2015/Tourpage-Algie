66dece0ada194f17722791200d20753fc7d24396
Algie Caballes <algie.developer@gmail.com>
2016-09-14 11:28:03 +0800

    handle tour image gallery data info

--- App/Modules/Vendor/Controllers/GalleryController.php
+++ App/Modules/Vendor/Controllers/GalleryController.php
@@ -23,6 +23,7 @@ class GalleryController extends VendorController {
     {
         $this->tag->setTitle('Gallery');
         $gallerries = \Tourpage\Models\ToursReviewGallery::query();
+        $gallerries->orderBy('\Tourpage\Models\ToursReviewGallery.galleryId DESC');
         $this->view->gallery = $this->getGalleries($gallerries->execute());
         $this->view->members = $this->getMembers();
     }
@@ -50,6 +51,7 @@ class GalleryController extends VendorController {
                 $data['images']['memberId'][] = $item->memberId;
                 $data['images']['galleryId'][] = $item->galleryId;
                 $data['images']['isShown'][] = $item->isShown;
+                $data['images']['tourname'][] = $this->getTourInfo($item->tourId);
             }
         }
         return $data;
@@ -62,8 +64,21 @@ class GalleryController extends VendorController {
         $members->leftJoin('\Tourpage\Models\ToursReviewGallery', 'b.memberId = \Tourpage\Models\Members.memberId', 'b');
         $result = $members->execute();
         foreach($result as $member) {
-            $data[$member->memberId] = "{$member->firstName} {$member->lastName}";
+            $data[$member->memberId]['name'] = "{$member->firstName} {$member->lastName}";
+            $data[$member->memberId]['email'] = $member->emailAddress;
         }
         return $data;
+    }
+    
+    private function getTourInfo($id)
+    {
+        $data = \Tourpage\Models\Tours::find([
+            "tourId = :tour_id:",
+            "bind" => [
+                ":tour_id" => $id
+            ]
+        ]);
+        $result = $data[0]->tourTitle;
+        return $result;
     }
 }
