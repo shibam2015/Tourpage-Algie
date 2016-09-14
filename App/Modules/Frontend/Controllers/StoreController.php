ae8f3a072385807b68773070cd7c458252d2072e
Algie Caballes <algie.developer@gmail.com>
2016-09-14 11:26:58 +0800

    handle image gallery information

--- App/Modules/Frontend/Controllers/StoreController.php
+++ App/Modules/Frontend/Controllers/StoreController.php
@@ -672,6 +672,7 @@ public function contactsAction($store = '', $content = '') {
             $data['images']['memberId'][] = $item->memberId;
             $data['images']['galleryId'][] = $item->galleryId;
             $data['images']['isShown'][] = $item->isShown;
+			$data['images']['tourname'][] = $this->getTourInfo($item->tourId);
         }
         return $data;
     }
@@ -683,8 +684,21 @@ public function contactsAction($store = '', $content = '') {
         $members->leftJoin('\Tourpage\Models\ToursReviewGallery', 'b.memberId = \Tourpage\Models\Members.memberId', 'b');
         $result = $members->execute();
         foreach($result as $member) {
-            $data[$member->memberId] = "{$member->firstName} {$member->lastName}";
+            $data[$member->memberId]['name'] = "{$member->firstName} {$member->lastName}";
+            $data[$member->memberId]['email'] = $member->emailAddress;
         }
         return $data;
+    }
+	
+	private function getTourInfo($id)
+    {
+        $data = \Tourpage\Models\Tours::find([
+            "tourId = :tour_id:",
+            "bind" => [
+                ":tour_id" => $id
+            ]
+        ]);
+        $result = $data[0]->tourTitle;
+        return $result;
     }
 }
