236b07d08ae19be187aaa4127fb201fcd6a90219
Algie Caballes <algie.developer@gmail.com>
2016-09-30 10:51:29 +0800

    register flash session in bootstrap for custom css

--- App/Modules/Frontend/Module.php
+++ App/Modules/Frontend/Module.php
@@ -80,6 +80,15 @@ class Module implements ModuleDefinitionInterface {
         $di->set('tag', function() {
             return new \Multiple\Frontend\Plugins\Tags();
         });
+        $di->set('flashSession', function(){
+            $flash = new \Phalcon\Flash\Session(array(
+                'error' => 'alert alert-danger',
+                'success' => 'alert alert-success',
+                'notice' => 'alert alert-info',
+                'warning' => 'alert alert-warning'
+            ));
+            return $flash;
+        });
     }
 
 }
