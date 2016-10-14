9ae0af6ffa9476475af98cdfd0a611355acbe850
Algie Caballes <algie.developer@gmail.com>
2016-10-14 10:59:00 +0800

    handle add to favorite bugs

--- public/elements/frontend/js/init.js
+++ public/elements/frontend/js/init.js
@@ -1,7 +1,8 @@
 function addToFavTour(tourId, vendorId) {
-    var actUrl = base64Encode(window.location.href);
+    var url = window.location.href.split("?")[0];
+    var actUrl = base64Encode(url+'?vid='+vendorId);
     //var tgtUrl = getUrl('tour/addtofav/' + vendorId + '/' + tourId + '?acturl=' + actUrl);
-    var tgtUrl = getUrl('tour/addtofav/' + tourId + '?acturl=' + actUrl);
+    var tgtUrl = getUrl('tour/addtofav/' + tourId + '?vendor_id='+vendorId+'&acturl=' + actUrl);
     return confirmRedirect(tgtUrl, 'This tour will be added to your favorite list');
 }
 
